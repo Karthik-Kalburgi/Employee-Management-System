93f84c5575e9fa2fba15e644dc55a12494f5ec3f
Karthik-Kalburgi <kalburgikarthik0105@gmail.com>
2024-11-30 16:27:27 +0530

    Done With props

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,21 @@ import AdminDashboard from "./Components/Auth/Dashboard/AdminDashboard";
 import { getLocalStorage, setLocalStorage } from "./Utils/LocalStorage";
 
 function App() {
-  useEffect(() => {
-    //setLocalStorage();
-    getLocalStorage();
-  });
-
+  const [user, setuser] = useState(null);
+  const handleLogin = (email, password) => {
+    if (email == "kk@gmail.com" && password == "123") {
+      setuser("admin");
+    } else if (email == "user@gmail.com" && password == "123") {
+      setuser("employee");
+    } else {
+      alert("Invalid crenditails");
+    }
+  };
 
   return (
     <>
-      <Login />
-
+      {!user ? <Login handleLogin={handleLogin} /> : ""};
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
       {/* <EmployeeDashboard /> */}
       {/* <AdminDashboard /> */}
     </>
--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 
-const Login = () => {
+const Login = ({handleLogin}) => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(`Email is ${email} `);
-    console.log(`Password is ${password} `);
+    // console.log(`Email is ${email} `);
+    // console.log(`Password is ${password} `);
+    handleLogin(email,password);
     setemail("");
     setpassword("");
   };
